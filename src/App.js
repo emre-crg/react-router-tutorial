75a23d03b7952b66c5ace098d8a6d56fbd64d05f
emre crg <vrtemre@gmail.com>
2020-05-13 19:25:34 +0300

    Route

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <Nav/>
-        <About/>
-        <Shop/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <di>
+    <h1>Home Page</h1>
+  </di>
+);
+
 
 export default App; 
